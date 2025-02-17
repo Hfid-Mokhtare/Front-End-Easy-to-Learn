cd15e3ef79fcab448b508f88df17ec511b598c55
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-02-17 15:36:40 +0100

    Update Lesson #01 : .js

--- Java_Script/Lesson #01 : .js
+++ Java_Script/Lesson #01 : .js
@@ -11,112 +11,112 @@
     <h1>Exercices de 1 a 13 :</h1>
     <script>
       //Exercice 5
-      // function isLeapYear(year)
-      // {
-      //   return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
-      // }
+      function isLeapYear(year)
+      {
+        return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+      }
 
-      // let yeartocheck = prompt(" enter the year : ");
-      // let year = parseInt(yeartocheck)
+      let yeartocheck = prompt(" enter the year : ");
+      let year = parseInt(yeartocheck)
 
-      // switch (isLeapYear(year))
-      // {
-      //   case true:
-      //   document.write( `<p>est une anne bissextile`);
-      //   break;
+      switch (isLeapYear(year))
+      {
+        case true:
+        document.write( `<p>est une anne bissextile`);
+        break;
 
-      //   case false:
-      //   document.write(`est une anne pas bissextile`);
-      //   break;
-      // }
+        case false:
+        document.write(`est une anne pas bissextile`);
+        break;
+      }
 
       // Exercie 6
 
-      // let age=prompt("please enter your age : ");
-      // let Iage=Number(age);
+      let age=prompt("please enter your age : ");
+      let Iage=Number(age);
 
-      // if(Iage<12)
-      // {
-      //   document.write(`enfant`)
-      // }
-      // else if(Iage<=17)
-      // {
-      //   document.write(`adolescent`)
-      // }
-      // else if(Iage<=64)
-      // {
-      //   document.write(`adulte`)
-      // }
-      // else
-      // {
-      //   document.write(`senior`)
-      // }
+      if(Iage<12)
+      {
+        document.write(`enfant`)
+      }
+      else if(Iage<=17)
+      {
+        document.write(`adolescent`)
+      }
+      else if(Iage<=64)
+      {
+        document.write(`adulte`)
+      }
+      else
+      {
+        document.write(`senior`)
+      }
 
       // Exercice 7
-      // var som=0;
-      // for(let i=1;i<=10;i++)
-      // {
-      //   som+=i;
-      // }
-      // document.write(`la some de 1 a 10 est : ${som}`)
+      var som=0;
+      for(let i=1;i<=10;i++)
+      {
+        som+=i;
+      }
+      document.write(`la some de 1 a 10 est : ${som}`)
 
       // Exercice 8
-      // let table=["blue","red", "ali","reda"]
+      let table=["blue","red", "ali","reda"]
 
-      // for (element in table)
-      // {
-      //   document.write(` ${table[element]}`)
-      // }
+      for (element in table)
+      {
+        document.write(` ${table[element]}`)
+      }
 
       // Exercice 9
-      // var chaine = prompt("enter votre chaine :");
-      // let voyel = ["a", "e", "y", "u", "i", "o"];
-      // let voyelNum = 0;
-      // for (let i = 0; i < chaine.length; i++) {
-      //   for (Vo of voyel) {
-      //     if (chaine[i].toLocaleLowerCase() === Vo) {
-      //       voyelNum++;
-      //       break;
-      //     }
-      //   }
-      // }
-      // document.write(`<p><strong>le nombre de voyel dons la chaine est : ${voyelNum}</strong></p>`)
+      var chaine = prompt("enter votre chaine :");
+      let voyel = ["a", "e", "y", "u", "i", "o"];
+      let voyelNum = 0;
+      for (let i = 0; i < chaine.length; i++) {
+        for (Vo of voyel) {
+          if (chaine[i].toLocaleLowerCase() === Vo) {
+            voyelNum++;
+            break;
+          }
+        }
+      }
+      document.write(`<p><strong>le nombre de voyel dons la chaine est : ${voyelNum}</strong></p>`)
 
       // Exercice 10
-      // var Num=parseInt(prompt("entrer un positive number : "));
-      // while(Num<=0)
-      // {
-      //   document.write(`entrer un number positive : `)
-      //   Num=parseInt(prompt("entrer un positive number : "));
-      // }
+      var Num=prompt("entrer un positive number : ");
+      while(Num<=0 || Num!=isNaN )
+      {
+        document.write(`entrer un number positive : `)
+        Num=parseInt(prompt("entrer un positive number : "));
+      }
 
       // Exercice 11
-      // let puissance=2;
-      // let number=0;
-      // let counter=1;
-      // while(number!=1024)
-      // {
-      //   number=puissance**counter;
-      //   document.write(`${number}</br>`);
-      //   counter++;
-      // }
+      let puissance=2;
+      let number=0;
+      let counter=1;
+      while(number!=1024)
+      {
+        number=puissance**counter;
+        document.write(`${number}</br>`);
+        counter++;
+      }
 
       // Exercice 12
-      // let voyel = ["A", "B", "C", "D", "E", "F"];
-      // voyel.forEach(function (lettre, index) {
-      //   document.write(`${lettre.charCodeAt()}</br>`);
-      // });
+      let voyel = ["A", "B", "C", "D", "E", "F"];
+      voyel.forEach(function (lettre, index) {
+        document.write(`${lettre.charCodeAt()}</br>`);
+      });
 
       // Exercice 13 :
-      // let voyel = ["A", "A", "A", "D", "E", "A"];
-      // var letter = prompt("entrer une letter : ");
-      // var counter=0;
-      // for (let ele of voyel) {
-      //   if (ele == letter) {
-      //     counter++;
-      //   }
-      // }
-      // document.write(`${counter}</br>`);
+      let voyel = ["A", "A", "A", "D", "E", "A"];
+      var letter = prompt("entrer une letter : ");
+      var counter=0;
+      for (let ele of voyel) {
+        if (ele == letter) {
+          counter++;
+        }
+      }
+      document.write(`${counter}</br>`);
 
      
     </script>
