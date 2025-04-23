81c3f23452c84cab865cd8234f2f4b45695c5cec
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-04-23 17:56:12 +0100

    Update Regex.js

--- Java_Script/Regex.js
+++ Java_Script/Regex.js
@@ -60,4 +60,24 @@
       let regMessage= /@,/ig;
       console.log(message.replaceAll(regMessage,"Mohamed Ali"));
 
+      
+
+
+let url1= "elzero.org";
+let url2= "http://elzero.org";
+let url3= "https://elzero.org";
+let url4= "https://www.elzero.org";
+let url5= "https://www.elzero.org:8080/articles.php?id=100&cat=topics";
+
+
+let reg = /(https?:\/\/(www.)?)?\w+.\w{3}(:+)?/gi;
+
+console.log(url1.match(reg));
+console.log(url2.match(reg));
+console.log(url3.match(reg));
+console.log(url4.match(reg));
+console.log(url5.match(reg));
+
+
+
     </script>
