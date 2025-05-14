0af77e2d8eaa98e2b951f00dd01356267693aa4f
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-05-14 11:29:58 +0100

    Create ex2.php

---
+++ PHP/files/ex2.php
@@ -0,0 +1,30 @@
+<?php
+
+
+$tableux=["ali","rida","yassin"];
+$nomFichier="myfile.txt";
+function EcrireContenuFichier($nomFichier,$tableux){
+    if (file_exists($nomFichier)){
+        $contenu=implode(",",$tableux);
+        print_r($contenu);
+        file_put_contents($nomFichier,$contenu);
+    }else{
+        echo 'Erreur : le fichier '.$nomFichier.' existe pas';
+    }
+}
+
+function afficherContenuFichier($nomFichier){
+    if (file_exists($nomFichier)){
+        $contenu=file_get_contents($nomFichier);
+        echo $contenu;
+
+    }else{
+        echo 'Erreur : le fichier '.$nomFichier.' existe pas';
+    }
+}
+
+EcrireContenuFichier("myfile.txt","hello my name is mohamed ali");
+afficherContenuFichier("myfile.txt");
+
+
+?>
