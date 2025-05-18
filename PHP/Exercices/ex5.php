c5c4c986852acf0e75441a920f5122b6defa555c
Mohamed Ali + <137907946+Hfid-Mokhtare@users.noreply.github.com>
2025-05-19 00:28:01 +0100

    Create ex5.php

---
+++ PHP/Exercices/ex5.php
@@ -0,0 +1,18 @@
+<?php
+
+function ali($name)
+{
+    echo "Hello $name <br>";
+}
+
+ali('Mohamed ali');
+
+
+function area( $length , $width){
+    return $length*$width;
+}
+
+echo 'the area is : '.area(10,20).'<br>';
+
+
+?>
